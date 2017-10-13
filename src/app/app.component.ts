519dd1cf1ee5abd23148857dec9a64692a6e2785
unknown <hebertsa07@gmail.com>
2017-10-12 20:20:35 -0400

    adding assignment files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Registration';
+  submit = false;
+  users = []
+  user = {
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:"",
+    confrimPW:"",
+    state:""
+  }
+  onSubmit(){
+    this.submit = true;
+    this.users.push(this.user)
+    console.log(this.user)
+    this.user = {
+      firstName:"",
+      lastName:"",
+      email:"",
+      password:"",
+      confrimPW:"",
+      state:""
+    }
+  }
 }
